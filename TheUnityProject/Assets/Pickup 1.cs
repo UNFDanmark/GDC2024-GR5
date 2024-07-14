210e8fb8ff172d4a9f8d52200484620ef85a5ac2
Deltager <teknisk+GDC2024-GR5@game.unf.dk>
2024-07-14 18:13:00 +0200

    Pickups i gang

--- TheUnityProject/Assets/Pickup 1.cs
+++ TheUnityProject/Assets/Pickup 1.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class Pickup1 : MonoBehaviour
 {
+    private Transform transform;
+
+    public GameObject pickupEffect;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
-    void Update()
+     void Update()
     {
+        /*
+        transform.Rotate(2f, 0f, 0f); //transform.up
+        //Få denne til at forsvinde når den collider med min character
         
+        */
+
     }
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Pickup();
+            
+            Destroy(gameObject);
+        }
+
+        void Pickup()
+        {
+            Instantiate(pickupEffect, transform.position, transform.rotation);
+            
+
+        }
+    }
+    
 }
